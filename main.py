a7f35d94ba69a592708f11c66caab3dbee1cce6b
eliza-ttt <73555386+eliza-ttt@users.noreply.github.com>
2020-12-12 21:50:04 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
-# Высота и ширина
+#width & height
 screen_width = 600
 screen_height = 600
 
@@ -10,7 +10,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Game')
 
 
-# Запуск кода
+#running
 run = True
 while run:
     for event in pygame.event.get():
