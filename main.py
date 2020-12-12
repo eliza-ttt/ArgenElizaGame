327fbdd7663065e10d3e113d6fecd87a2edd5c30
eliza-ttt <73555386+eliza-ttt@users.noreply.github.com>
2020-12-13 00:14:27 +0600

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Game')
 
 bg = (69, 69, 81)
+block_aqua = (230, 190, 231)
+block_ocean= (206, 100, 210)
+block_bottom= (146, 32, 149)
 
 #running
 run = True
